b1213c4c5212b0f58bbe780c1106211fdf351caf
Milos Rujevic <milosrujevic@gmail.com>
2022-10-15 14:09:45 +0200

    Add basic meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,31 @@ import MobileHomePage from './pages/mobile/MobileHomePage'
 import WebAppLayout from './layout/web/WebAppLayout'
 
 function App() {
-  return isMobile ?
-    <MobileHomePage /> :
-    <WebAppLayout />
+  if (isMobile) {
+    return (
+        <div>
+          <MetaTags>
+            <title>revenize.co</title>
+            <meta name="description" content="We help brands succeed on Amazon" />
+            <meta property="og:title" content="revenize" />
+            <meta property="og:image" content="https://link.storjshare.io/juj6zd243wmql4m2by2pea2zonkq/qraft-nft%2Frevenize-meta-picture.png" />
+          </MetaTags>
+          <MobileHomePage />
+        </div>
+    )
+  } else {
+    return (
+        <div>
+          <MetaTags>
+            <title>Page 1</title>
+            <meta name="description" content="Some description." />
+            <meta property="og:title" content="MyApp" />
+            <meta property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <WebAppLayout />
+        </div>
+    )
+  }
 }
 
 export default App
